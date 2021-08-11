2f36ba35b35eda5572c56de9d179a26ebb0050c1
Sean Kristian Anderson <64885114+seanka@users.noreply.github.com>
2021-08-12 01:04:44 +0700

    fixed secondPage : changed widget trees, added more contents

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, prefer_const_constructors
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:moovi/constant.dart';
@@ -12,7 +12,7 @@ class SecondPage extends StatelessWidget{
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.only(left: 10),
+            padding: EdgeInsets.only(left: 20, bottom: 75),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -20,30 +20,80 @@ class SecondPage extends StatelessWidget{
                 Stack(
                   children: <Widget> [
                     Padding(
-                      padding: EdgeInsets.only(left: 25),
-                      child: Container(
-                        width: 500,
-                        height: 500,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(25),
+                        padding: EdgeInsets.only(left: 25),
+                        child: Container(
+                          width: 500,
+                          height: 475,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(25),
+                              ),
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/posterTheLittlePrince.jpg'),
+                                fit: BoxFit.cover,
+                              )
                           ),
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/posterTheLittlePrince.jpg'),
-                            fit: BoxFit.cover,
-                          )
-                        ),
-                      )
+                        )
                     ),
                     TextButton(
                       onPressed: () {Navigator.pop(context);},
+                      style: ButtonStyle(
+                          overlayColor: MaterialStateProperty.all(Colors.transparent)
+                      ),
                       child: Text('back', style: ThemeText.mainText,),
                     ),
                   ],
                 ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor "
+                      "in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                  style: ThemeText.mainText,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Wrap(
+                      spacing: 72,
+                      children: <Widget> [
+                        Text('RATING ', style: ThemeText.mainText,),
+                        Text('7.5 / 10', style: ThemeText.mainText,),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Wrap(
+                      spacing: 50,
+                      children: <Widget> [
+                        Text('DIRECTOR ', style: ThemeText.mainText,),
+                        Text('7.5 / 10', style: ThemeText.mainText,),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Wrap(
+                      spacing: 90,
+                      children: <Widget> [
+                        Text('CAST ', style: ThemeText.mainText,),
+                        Text('7.5 / 10', style: ThemeText.mainText,),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
