099d790cd7772f34d1327c9e99f7f3948ef336c1
Sean Kristian Anderson <64885114+seanka@users.noreply.github.com>
2021-08-12 12:13:10 +0700

    fixed typo in movieData

--- lib/movieData.dart
+++ lib/movieData.dart
@@ -24,7 +24,7 @@ var movieDataList = [
       cover: 'assets/images/posterTheRevenant.jpg',
       title: 'The Revenant',
       genre: '2021 . Dark',
-      description: "Lorem ipsum dolor sit amet, co7nsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor "
           "in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
       rating: '8.5/ 10',
